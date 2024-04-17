8de48d940f83c0a2014f72d42abcc5166bcfed53
huseyinsoyturk <soyturk.huseyin61@gmail.com>
2024-04-17 16:53:12 +0300

    map eklendi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
 import './App.css';
+import iller from './iller.json';
+import yemekler from './yemekler.json';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import "ol/ol.css";
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import Style from 'ol/style/Style';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+import Text from 'ol/style/Text';
+import { Point } from 'ol/geom';
+
+
 
 function App() {
+  const mapRef = useRef()
+  const [mapState, setMapState] = useState()
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [showModal, setshowModal] = useState()
+
+  const hlLayer = new VectorLayer({
+    source: new VectorSource({
+      format: new GeoJSON(),
+    }),
+    style: new Style({
+      fill: new Fill({
+        color: '#580f27'
+      })
+    })
+  })
+
+  const geoJson = new GeoJSON({ dataProjection: "EPSG:4326", featureProjection: "EPSG:3857" })
+
+  useEffect(() => {
+    let map;
+    if (mapRef) {
+      map = new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+          new VectorLayer({
+            source: new VectorSource({
+              format: new GeoJSON(),
+              features: new GeoJSON().readFeatures(iller, { dataProjection: 'EPSG:4326', featureProjection: "EPSG:3857" }),
+            }),
+            style: new Style({
+              fill: new Fill({
+                color: '#73b2e090'
+              }),
+              stroke: new Stroke({
+                color: '#580f2790',
+                width: 3
+              }),
+            })
+          }),
+          hlLayer
+        ],
+        target: 'map',
+        view: new View({
+          center: [3935795.1369, 4696142.9211],
+          zoom: 7,
+        }),
+      });
+      setMapState(map)
+    }
+
+    return () => {
+      if (map) {
+        map.dispose();
+        setMapState();
+      }
+    }
+  }, [mapRef])
+
+  const handleClick = () => {
+    setButtonDisabled(true)
+    let number;
+    let data;
+    let interval = setInterval(() => {
+      const randomNumber = Math.floor((Math.random() * 81) + 1);
+      number = randomNumber;
+      const source = mapState.getAllLayers()[2].getSource()
+      source.clear();
+      data = iller.features.find(obj => obj.id === number);
+      const feature = geoJson.readFeature(data)
+      source.addFeature(feature);
+    }, 200);
+
+    setTimeout(() => {
+      setButtonDisabled(false)
+      clearInterval(interval)
+      console.log(data);
+      const yemek = yemekler.iller.find(obj => obj.il === data.properties.name);
+      console.log(yemek);
+      setshowModal(yemek)
+    }, 2000);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={mapRef} id="map" className="map">
+      <button disabled={buttonDisabled || showModal} className="btn" onClick={handleClick}>Çarkı Çevir</button>
+      {showModal && <div id="myModal" class="modal">
+        <div class="modal-content">
+          <div onClick={() => { setshowModal() }} class="close">&times;</div>
+          <h1>{showModal.il}</h1>
+          <div>Tebrikler {showModal.en_unlu_yemek} kazandiniz!!!</div>
+        </div>
+      </div>}
     </div>
   );
 }
